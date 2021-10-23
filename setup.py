c812b52af2576d1dcac102fa00a0e3cd9f1be469
Nikhil <nikhil@policyengine.org>
2021-10-23 22:58:42 +0100

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-Tools",
-    version="0.2.0",
+    version="0.1.0",
     author="PolicyEngine",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/openfisca-tools",
