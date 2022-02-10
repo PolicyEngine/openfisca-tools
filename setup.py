ca2ad2a5ae0bc66abb80601039b1cbcb9b222760
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-02-10 14:56:51 +0000

    Versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-Tools",
-    version="0.2.4",
+    version="0.3.0",
     author="PolicyEngine",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/openfisca-tools",
